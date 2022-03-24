3e714a6b9989ed5b1e45c5752aa87126049bba06
郑佳雯 <zhengjiawen@192.168.1.138>
2022-03-24 20:55:05 +0000

    part11_heroku_update4

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const app = express()
 // eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000
 
-const HOST = '0.0.0.0'
-
 app.use(express.static('dist'))
 
-app.listen(PORT, HOST, () => {
+app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
 })
